baf1773fa09e62f058754b75e378ab29adfa2274
bottyan.hammer <bottyan.hammer@gmail.com>
2024-09-13 12:31:16 +0200

    Tömb-kezelés

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ A JS gyengén típusos nyelv: ==, 3+"4" -> '34',  3*"4" -> 12
 const button = document.querySelector("ul li:last-child a");
 button.textContent = "Új szöveg";
 button.style.backgroundColor = "rgb(200, 20, 20)";
-button.style.display = "none";
+// button.style.display = "none";
 console.log(button);
 
 document.querySelector(".card").style.backgroundColor = "#123456";
@@ -40,3 +40,26 @@ const text = "Ez egy hosszú szöveg";
 paragraph.textContent = text;
 paragraph.style.fontSize = "1.2rem";
 floatDiv.appendChild(paragraph);
+/*
+Array (tömb) ~ Python lista (dinamikus, heterogén)
+const t = [];  NE: const t = new Array();
+Elemek elérése: t[0], t.at(-1)
+t.slice(start, end) end nélkül a tömb végéig megy. (Szeletelő) ~ [::]
+t.splice(start, deletedCount, inserted elements)
+const newT = t.tospliced(start, deletedCount, inserted elements);
+t.indexOf(20)  --> az elem indexét adja vissza.
+t.toString()  ---> '10,20,30'
+t.join() ---> '10,20,30'     t.join('')   ---> '102030'   
+*/
+
+const pElements = document.querySelectorAll("p");
+//pElements.at(-1).style.fontFamily = "Arial";
+pElements[pElements.length-1].style.color = "hsl(100, 60%, 70%)";
+Array.from(pElements).splice(0, 3);
+// Az Array.from() metódus tömböt készít a nodeList-ből. A NodeList nem tömb!!!
+floatDiv.innerHTML = "";
+console.log(Array.from(pElements));
+for (let element of pElements) {
+    console.log(element.innerHTML);
+    floatDiv.innerHTML += element.innerHTML;
+}
